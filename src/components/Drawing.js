34411a96ec678cc588644d7e1523e890e9616bcb
lirik <kirill.isachenko@gmail.com>
2024-07-05 15:13:33 +0700

    remove artwork modal

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 
 const Drawing = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
   const { src, title, width, height } = props;
   return (
-    <>
-      {isOpen && (
-        <div className="full-size-image">
-          <Image src={src} alt={title} width={width} height={height} onClick={() => setIsOpen(!isOpen)} />
-        </div>
-      )}
-      <Image
-        src={src}
-        alt={title}
-        className="drawing"
-        width={width}
-        height={height}
-        onClick={() => setIsOpen(!isOpen)}
-      />
-    </>
+    <Image
+      src={src}
+      alt={title}
+      className="drawing"
+      width={width}
+      height={height}
+    />
   );
 };
 
